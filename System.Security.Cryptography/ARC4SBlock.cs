15356492684cec389394334d40f851d82eadd8d8
ng256 <90511962+ng256@users.noreply.github.com>
2021-12-02 23:55:57 +0300

    Update ARC4SBlock.cs

--- System.Security.Cryptography/ARC4SBlock.cs
+++ System.Security.Cryptography/ARC4SBlock.cs
@@ -2,7 +2,11 @@ using System.Runtime.InteropServices;
 
 namespace System.Security.Cryptography
 {
-	[Serializable]
+        /// <summary>
+        /// Represents the initial state of the cryptographic algorithm <see cref = "ARC4" />.
+        /// This class is not inherited.
+        /// </summary> 
+        [Serializable]
 	[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 256)]
 	public sealed class ARC4SBlock : IDisposable
 	{
@@ -10,16 +14,58 @@ namespace System.Security.Cryptography
 
 		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
 		private byte[] _bytes = new byte[256];
-
-		internal static bool ValidBytes(byte[] bytes)
+		
+                /// <summary>
+                /// Initializes an instance <see cref = "ARC4SBlock" />,
+                /// filled with pseudo-random values,
+                /// using the linear congruential random.
+                /// </summary>
+                /// <returns> Instance <see cref = "ARC4SBlock" />. </returns> 
+		public static ARC4SBlock GenerateRandom()
+		{
+			byte[] bytes = new byte[256];
+			byte[] random = new byte[4];
+			CryptoProvider.InternalRng.GetBytes(random);
+			int x = BitConverter.ToInt32(random, 0) % 256;
+			int a = 2;
+			while ((a - 1) % 4 != 0)
+			{
+				random = new byte[4];
+				CryptoProvider.InternalRng.GetBytes(random);
+				int rnd = BitConverter.ToInt32(random, 0);
+				a = 5 + rnd % 246;
+			}
+			for (int i = 0; i < 256; i++)
+			{
+				bytes[i] = (byte)(x = (a * x + 17) % 256);
+			}
+			return new ARC4SBlock(bytes);
+		}
+		
+                /// <summary>
+                /// Initializes an instance <see cref = "ARC4SBlock" />,
+                /// using the specified values.
+                /// </summary>
+                /// <param name = "bytes"> The initialization vector <see cref = "ARC4SBlock" />, must be filled with 256 non-duplicate values. </param>
+                /// <returns> Instance <see cref = "ARC4SBlock" />. </returns> 
+		public static ARC4SBlock FromBytes(params byte[] bytes)
+		{
+			if (!ValidBytes(bytes))
+			{
+				throw new DuplicateWaitObjectException("bytes");
+			}
+			return new ARC4SBlock(bytes);
+		}
+                
+                internal static bool ValidBytes(byte[] bytes)
 		{
 			if (bytes == null || bytes.Length != 256)
 			{
 				return false;
 			}
-			for (byte i = 0; i <= byte.MaxValue; i = (byte)(i + 1))
+			for (int i = 0; i < 256; i++)
 			{
-				for (byte j = i; j <= byte.MaxValue; j = (byte)(j + 1))
+				for (int j = i; j < 256; j++)
 				{
 					if (bytes[i] == bytes[j])
 					{
@@ -44,36 +90,6 @@ namespace System.Security.Cryptography
 			Array.Copy(bytes, _bytes, 256);
 		}
 
-		public static ARC4SBlock GenerateRandom()
-		{
-			byte[] bytes = new byte[256];
-			byte[] random = new byte[4];
-			CryptoProvider.InternalRng.GetBytes(random);
-			int x = BitConverter.ToInt32(random, 0) % 256;
-			int a = 2;
-			while ((a - 1) % 4 != 0)
-			{
-				random = new byte[4];
-				CryptoProvider.InternalRng.GetBytes(random);
-				int rnd = BitConverter.ToInt32(random, 0);
-				a = 5 + rnd % 246;
-			}
-			for (int i = 0; i < 256; i++)
-			{
-				bytes[i] = (byte)(x = (a * x + 17) % 256);
-			}
-			return new ARC4SBlock(bytes);
-		}
-
-		public static ARC4SBlock FromBytes(params byte[] bytes)
-		{
-			if (!ValidBytes(bytes))
-			{
-				throw new DuplicateWaitObjectException("bytes");
-			}
-			return new ARC4SBlock(bytes);
-		}
-
 		public static implicit operator byte[](ARC4SBlock sblock)
 		{
 			byte[] bytes = new byte[256];
@@ -90,6 +106,7 @@ namespace System.Security.Cryptography
 			return new ARC4SBlock(bytes);
 		}
 
+                /// <inheritdoc cref="IDisposable.Dispose"/>
 		public void Dispose()
 		{
 			CryptoProvider.EraseArray(_bytes);
