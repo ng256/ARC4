004da53810ef5f2706196e55212fa3a6465c7bc1
Pavel Bashkardin <90511962+ng256@users.noreply.github.com>
2024-09-17 15:40:20 +0300

    Update ARC4SBlock.cs
    Optimize bytes validation

--- System.Security.Cryptography/ARC4SBlock.cs
+++ System.Security.Cryptography/ARC4SBlock.cs
@@ -3,99 +3,99 @@ using System.Runtime.InteropServices;
 
 namespace System.Security.Cryptography
 {
-	/// <summary>
-	///     Represents the initial state of the cryptographic algorithm <see cref = "ARC4" />.
-	///     This class could not be inherited.
-	/// </summary> 
-	[Serializable]
-	[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 256)]
-	public sealed class ARC4SBlock : IDisposable, ICloneable
-	{
-		private static readonly byte[] _A =
-		{
-			0x09, 0x0D, 0x11, 0x15, 0x19, 0x1D, 0x21, 0x25,
-			0x29, 0x2D, 0x31, 0x35, 0x39, 0x3D, 0x41, 0x45,
-			0x49, 0x4D, 0x51, 0x55, 0x59, 0x5D, 0x61, 0x65,
-			0x69, 0x6D, 0x71, 0x75, 0x79, 0x7D, 0x81, 0x85,
-			0x89, 0x8D, 0x91, 0x95, 0x99, 0x9D, 0xA1, 0xA5,
-			0xA9, 0xAD, 0xB1, 0xB5, 0xB9, 0xBD, 0xC1, 0xC5,
-			0xC9, 0xCD, 0xD1, 0xD5, 0xD9, 0xDD, 0xE1, 0xE5,
-			0xE9, 0xED, 0xF1, 0xF5, 0xF9
-		};
+    /// <summary>
+    ///     Represents the initial state of the cryptographic algorithm <see cref = "ARC4" />.
+    ///     This class could not be inherited.
+    /// </summary> 
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 256)]
+    public sealed class ARC4SBlock : IDisposable, ICloneable
+    {
+        private static readonly byte[] _A =
+        {
+            0x09, 0x0D, 0x11, 0x15, 0x19, 0x1D, 0x21, 0x25,
+            0x29, 0x2D, 0x31, 0x35, 0x39, 0x3D, 0x41, 0x45,
+            0x49, 0x4D, 0x51, 0x55, 0x59, 0x5D, 0x61, 0x65,
+            0x69, 0x6D, 0x71, 0x75, 0x79, 0x7D, 0x81, 0x85,
+            0x89, 0x8D, 0x91, 0x95, 0x99, 0x9D, 0xA1, 0xA5,
+            0xA9, 0xAD, 0xB1, 0xB5, 0xB9, 0xBD, 0xC1, 0xC5,
+            0xC9, 0xCD, 0xD1, 0xD5, 0xD9, 0xDD, 0xE1, 0xE5,
+            0xE9, 0xED, 0xF1, 0xF5, 0xF9
+        };
 
-		private static readonly byte[] _C =
-		{
-			0x05, 0x07, 0x0B, 0x0D, 0x11, 0x13, 0x17, 0x1D,
-			0x1F, 0x25, 0x29, 0x2B, 0x2F, 0x35, 0x3B, 0x3D,
-			0x43, 0x47, 0x49, 0x4F, 0x53, 0x59, 0x61, 0x65,
-			0x67, 0x6B, 0x6D, 0x71, 0x7F, 0x83, 0x89, 0x8B,
-			0x95, 0x97, 0x9D, 0xA3, 0xA7, 0xAD, 0xB3, 0xB5,
-			0xBF, 0xC1, 0xC5, 0xC7, 0xD3, 0xDF, 0xE3, 0xE5,
-			0xE9, 0xEF, 0xF1, 0xFB
-		};
+        private static readonly byte[] _C =
+        {
+            0x05, 0x07, 0x0B, 0x0D, 0x11, 0x13, 0x17, 0x1D,
+            0x1F, 0x25, 0x29, 0x2B, 0x2F, 0x35, 0x3B, 0x3D,
+            0x43, 0x47, 0x49, 0x4F, 0x53, 0x59, 0x61, 0x65,
+            0x67, 0x6B, 0x6D, 0x71, 0x7F, 0x83, 0x89, 0x8B,
+            0x95, 0x97, 0x9D, 0xA3, 0xA7, 0xAD, 0xB3, 0xB5,
+            0xBF, 0xC1, 0xC5, 0xC7, 0xD3, 0xDF, 0xE3, 0xE5,
+            0xE9, 0xEF, 0xF1, 0xFB
+        };
 
-		public static readonly ARC4SBlock DefaultSBlock = new ARC4SBlock();
+        public static readonly ARC4SBlock DefaultSBlock = new ARC4SBlock();
 
-		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
-		private byte[] _bytes = new byte[256];
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        private byte[] _bytes = new byte[256];
 
         /// <summary>
-		///     Initializes an instance <see cref = "ARC4SBlock" />,
-		///     filled with pseudo-random values,
-		///     using the linear congruential random.
-		/// </summary>
-		/// <returns>
-		///     Instance <see cref = "ARC4SBlock" />.
-		/// </returns> 
-		public static ARC4SBlock GenerateRandom()
-		{
+	///     Initializes an instance <see cref = "ARC4SBlock" />,
+	///     filled with pseudo-random values,
+	///     using the linear congruential random.
+	/// </summary>
+	/// <returns>
+	///     Instance <see cref = "ARC4SBlock" />.
+	/// </returns> 
+	public static ARC4SBlock GenerateRandom()
+        {
             byte[] random = new byte[4];
-			CryptoProvider.InternalRng.GetBytes(random);
-			int r = random[0];
-			int x = random[1];
-			int a = _A[random[2] % _A.Length];
-			int c = _C[random[3] % _C.Length];
+            CryptoProvider.InternalRng.GetBytes(random);
+            int r = random[0];
+            int x = random[1];
+            int a = _A[random[2] % _A.Length];
+            int c = _C[random[3] % _C.Length];
 
             return new ARC4SBlock(x, a, c, r);
-		}
-
-		/// <summary>
-		///     Initializes an instance <see cref = "ARC4SBlock" />,
-		///     using the specified values.
-		/// </summary>
-		/// <param name = "bytes">
-		///     The initialization vector <see cref = "ARC4SBlock" />,
-		///     must be filled with 256 non-duplicate values.
-		/// </param>
-		/// <returns>
-		///     Instance <see cref = "ARC4SBlock" />.
-		/// </returns> 
-		public static ARC4SBlock FromBytes(params byte[] bytes)
-		{
-			if (!ValidBytes(bytes))
-			{
-				throw new DuplicateWaitObjectException("bytes");
-			}
-
-			return new ARC4SBlock(bytes);
-		}
+        }
 
         /// <summary>
-		///     Initializes an instance <see cref = "ARC4SBlock" />,
-		///     using the specified salt.
-		/// </summary>
-		/// <param name = "salt">
-		///     Salt for the LCR algorithm. It must contain at least 4 bytes.
-		/// </param>
-		/// <returns>
-		///     Instance <see cref = "ARC4SBlock" />.
-		/// </returns> 
-		public static ARC4SBlock FromSalt(params byte[] salt)
-		{
-			if (salt.Length < 4)
-			{
-				throw new DuplicateWaitObjectException("bytes");
-			}
+        ///     Initializes an instance <see cref = "ARC4SBlock" />,
+        ///     using the specified values.
+        /// </summary>
+        /// <param name = "bytes">
+        ///     The initialization vector <see cref = "ARC4SBlock" />,
+        ///     must be filled with 256 non-duplicate values.
+        /// </param>
+        /// <returns>
+        ///     Instance <see cref = "ARC4SBlock" />.
+        /// </returns> 
+        public static ARC4SBlock FromBytes(params byte[] bytes)
+        {
+            if (!ValidBytes(bytes))
+            {
+                throw new DuplicateWaitObjectException("bytes");
+            }
+
+            return new ARC4SBlock(bytes);
+        }
+
+        /// <summary>
+        ///     Initializes an instance <see cref = "ARC4SBlock" />,
+        ///     using the specified salt.
+        /// </summary>
+        /// <param name = "salt">
+        ///     Salt for the LCR algorithm. It must contain at least 4 bytes.
+        /// </param>
+        /// <returns>
+        ///     Instance <see cref = "ARC4SBlock" />.
+        /// </returns> 
+        public static ARC4SBlock FromSalt(params byte[] salt)
+        {
+            if (salt.Length < 4)
+            {
+                throw new DuplicateWaitObjectException("bytes");
+            }
 
             int r = salt[3];
             int x = salt[2];
@@ -111,7 +111,7 @@ namespace System.Security.Cryptography
         /// <param name="sblock">
         ///     Instance <see cref="ARC4SBlock"/> for converting.
         /// </param>
-        public static implicit operator byte[] (ARC4SBlock sblock)
+        public static implicit operator byte[](ARC4SBlock sblock)
         {
             if (sblock == null)
             {
@@ -119,7 +119,7 @@ namespace System.Security.Cryptography
             }
             if (sblock._bytes == null)
             {
-                throw  new ObjectDisposedException(nameof(sblock),
+                throw new ObjectDisposedException(nameof(sblock),
                     DefaultFormatter.GetMessage("ObjectDisposed_Generic"));
             }
 
@@ -149,21 +149,32 @@ namespace System.Security.Cryptography
         }
 
         // Checks that all 256 values should not be duplicated.
-        internal static bool ValidBytes(byte[] bytes)
+        internal static unsafe bool ValidBytes(byte[] bytes)
         {
-            if (bytes == null || bytes.Length != 256)
-            {
+            // Check if the length of the bytes array is equal to the sblock size.
+            const int bytesSize = 256;
+            if (bytes == null || bytes.Length != bytesSize)
                 return false;
-            }
 
-            for (int i = 0; i < 256; i++)
+            fixed (byte* bytesPtr = bytes)
             {
-                for (int j = i + 1; j < 256; j++)
+                const int seenSize = 8;
+                int* seenPtr = stackalloc int[seenSize];
+                for (int seenIndex = 0; seenIndex < seenSize; seenIndex++)
+                    seenPtr[seenIndex] = 0;
+
+                for (int byteIndex = 0; byteIndex < bytesSize; byteIndex++)
                 {
-                    if (bytes[i] == bytes[j])
-                    {
+                    // Check if the current byte has already been seen.
+                    byte currentByte = bytesPtr[byteIndex];
+                    int mask = 1 << (currentByte & 0x1F);
+                    int offset = currentByte >> 5;
+
+                    if ((seenPtr[offset] & mask) != 0)
                         return false;
-                    }
+
+                    // Mark the current byte as seen.
+                    seenPtr[offset] |= mask;
                 }
             }
 
@@ -172,18 +183,18 @@ namespace System.Security.Cryptography
 
         // Default S-Block.
         private ARC4SBlock()
-		{
-			for (int i = 0; i < 256; i++)
-			{
-				_bytes[i] = (byte) i;
-			}
-		}
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                _bytes[i] = (byte)i;
+            }
+        }
 
         // Specified S-Block.
         internal ARC4SBlock(byte[] bytes)
-		{
+        {
             Array.Copy(bytes, _bytes, 256);
-		}
+        }
 
         // Random S-Block.
         internal ARC4SBlock(int x, int a, int c, int r)
@@ -195,14 +206,14 @@ namespace System.Security.Cryptography
             }
         }
 
-		/// <inheritdoc cref="IDisposable.Dispose"/>
-		public void Dispose()
+        /// <inheritdoc cref="IDisposable.Dispose"/>
+        public void Dispose()
         {
             if (_bytes == null) return;
-			CryptoProvider.EraseArray(ref _bytes);
-			_bytes = null;
-			GC.SuppressFinalize(this);
-		}
+            CryptoProvider.EraseArray(ref _bytes);
+            _bytes = null;
+            GC.SuppressFinalize(this);
+        }
 
         /// <inheritdoc cref="ICloneable.Clone"/>
         public object Clone()
