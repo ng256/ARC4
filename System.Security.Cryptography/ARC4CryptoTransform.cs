2a74601f3b0d7f4dc5c319238d52d5c242fff167
Pavel Bashkardin <90511962+ng256@users.noreply.github.com>
2024-12-11 12:28:43 +0300

    Update ARC4CryptoTransform.cs

--- System.Security.Cryptography/ARC4CryptoTransform.cs
+++ System.Security.Cryptography/ARC4CryptoTransform.cs
@@ -2,13 +2,13 @@ using static System.ComponentModel.AssemblyMessageFormatter;
 
 namespace System.Security.Cryptography
 {
-	/// <summary>
-	///     Performs cryptographic transformation of data using the <see cref = "ARC4CryptoProvider" /> algorithm.
-	///     This class could not be inherited.
-	/// </summary> 
-	public sealed class ARC4CryptoTransform : ICryptoTransform
-	{
-		private bool _disposed = false;
+    /// <summary>
+    ///     Performs cryptographic transformation of data using the <see cref = "ARC4CryptoProvider" /> algorithm.
+    ///     This class could not be inherited.
+    /// </summary> 
+    public sealed class ARC4CryptoTransform : ICryptoTransform
+    {
+        private bool _disposed = false;
         private ARC4CryptoProvider _arc4;
 
         /// <summary>
@@ -24,35 +24,32 @@ namespace System.Security.Cryptography
                 : _arc4.State;
 
         /// <inheritdoc cref="ICryptoTransform.InputBlockSize"/>
-		public int InputBlockSize => 1;
+        public int InputBlockSize => 1;
 
-		/// <inheritdoc cref="ICryptoTransform.OutputBlockSize"/>
-		public int OutputBlockSize => 1;
+        /// <inheritdoc cref="ICryptoTransform.OutputBlockSize"/>
+        public int OutputBlockSize => 1;
 
-		/// <inheritdoc cref="ICryptoTransform.CanTransformMultipleBlocks"/>
-		public bool CanTransformMultipleBlocks => true;
+        /// <inheritdoc cref="ICryptoTransform.CanTransformMultipleBlocks"/>
+        public bool CanTransformMultipleBlocks => true;
 
-		/// <inheritdoc cref="ICryptoTransform.CanReuseTransform"/>
-		public bool CanReuseTransform => true;
+        /// <inheritdoc cref="ICryptoTransform.CanReuseTransform"/>
+        public bool CanReuseTransform => true;
 
         /// <summary>
-		///     Initializes a new instance <see cref = "ARC4CryptoTransform" />, using the specified parameters.
-		/// </summary>
-		/// <param name = "key">
-		///     The secret key to be used for the symmetric algorithm.
-		/// </param>
-		public ARC4CryptoTransform(byte[] key)
-		{
+        ///     Initializes a new instance <see cref = "ARC4CryptoTransform" />, using the specified parameters.
+        /// </summary>
+        /// <param name = "key">
+        ///     The secret key to be used for the symmetric algorithm.
+        /// </param>
+        public ARC4CryptoTransform(byte[] key)
+        {
             if (key == null)
-			{
-				throw new ArgumentNullException(nameof(key));
-			}
-			if (key.Length == 0)
-			{
-				throw new ArgumentException(null, nameof(key));
-			}
-			_arc4 = new ARC4CryptoProvider(key);
-		}
+                throw new ArgumentNullException(nameof(key));
+            if (key.Length == 0)
+                throw new ArgumentException(null, nameof(key));
+                
+            _arc4 = new ARC4CryptoProvider(key);
+        }
 
         /// <summary>
         ///     Initializes a new instance <see cref = "ARC4CryptoTransform" />
@@ -65,25 +62,18 @@ namespace System.Security.Cryptography
         ///     Initialization vector.
         /// </param> 
         public ARC4CryptoTransform(byte[] key, byte[] iv)
-		{
+        {
             if (key == null)
-			{
-				throw new ArgumentNullException(nameof(key));
-			}
-			if (key.Length == 0)
-			{
-				throw new ArgumentException(null, nameof(key));
-			}
-			if (iv == null)
-			{
-				throw new ArgumentNullException(nameof(iv));
-			}
-			if (!ARC4SBlock.ValidBytes(iv))
-			{
-				throw new ArgumentException(null, nameof(iv));
-			}
-			_arc4 = new ARC4CryptoProvider(key, iv);
-		}
+                throw new ArgumentNullException(nameof(key));
+            if (key.Length == 0)
+                throw new ArgumentException(null, nameof(key));
+            if (iv == null)
+                throw new ArgumentNullException(nameof(iv));
+            if (!ARC4SBlock.ValidBytes(iv))
+               throw new ArgumentException(null, nameof(iv));
+               
+            _arc4 = new ARC4CryptoProvider(key, iv);
+        }
 
         /// <summary>
         ///     Initializes a new instance <see cref = "ARC4CryptoTransform" />
@@ -97,76 +87,55 @@ namespace System.Security.Cryptography
         ///     used as the initial state of the ARC4 algorithm.
         /// </param> 
         public ARC4CryptoTransform(byte[] key, ARC4SBlock sblock)
-		{
+        {
             if (key == null)
-			{
-				throw new ArgumentNullException(nameof(key));
-			}
-			if (key.Length == 0)
-			{
-				throw new ArgumentException(null, nameof(key));
-			}
-			if (sblock == null)
-			{
-				throw new ArgumentNullException(nameof(sblock));
-			}
-			_arc4 = new ARC4CryptoProvider(key, sblock);
-		}
+                throw new ArgumentNullException(nameof(key));
+            if (key.Length == 0)
+                throw new ArgumentException(null, nameof(key));
+            if (sblock == null)
+                throw new ArgumentNullException(nameof(sblock));
+                
+            _arc4 = new ARC4CryptoProvider(key, sblock);
+        }
 
-		/// <inheritdoc cref="ICryptoTransform.TransformBlock"/>
- 		public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
-		{
+        /// <inheritdoc cref="ICryptoTransform.TransformBlock"/>
+        public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+        {
             if (_disposed)
-            {
                 throw new ObjectDisposedException(nameof(ARC4CryptoTransform),
                     DefaultFormatter.GetMessage("ObjectDisposed_Generic"));
-            }
             if (inputBuffer == null)
-			{
-				throw new ArgumentNullException(nameof(inputBuffer));
-			}
-			if (outputBuffer == null)
-			{
-				throw new ArgumentNullException(nameof(outputBuffer));
-			}
-			if (inputOffset < 0)
-			{
-				throw new ArgumentOutOfRangeException(nameof(inputOffset), inputOffset, null);
-			}
-			if (inputCount <= 0 || inputCount % InputBlockSize != 0 || inputCount > inputBuffer.Length || inputBuffer.Length - inputCount < inputOffset)
-			{
-				throw new ArgumentException(null, nameof(inputCount));
-			}
-			Array.Copy(inputBuffer, inputOffset, outputBuffer, outputOffset, inputCount);
-			_arc4.Cipher(outputBuffer, outputOffset, inputCount);
-			return inputCount;
-		}
+                throw new ArgumentNullException(nameof(inputBuffer));
+            if (outputBuffer == null)
+                throw new ArgumentNullException(nameof(outputBuffer));
+            if (inputOffset < 0)
+                throw new ArgumentOutOfRangeException(nameof(inputOffset), inputOffset, null);
+            if (inputCount <= 0 || inputCount % InputBlockSize != 0 || inputCount > inputBuffer.Length || inputBuffer.Length - inputCount < inputOffset)
+                throw new ArgumentException(null, nameof(inputCount));
+                
+            Array.Copy(inputBuffer, inputOffset, outputBuffer, outputOffset, inputCount);
+            _arc4.Cipher(outputBuffer, outputOffset, inputCount);
+            return inputCount;
+        }
 
-		/// <inheritdoc cref="ICryptoTransform.TransformFinalBlock"/>
-		public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
-		{
+        /// <inheritdoc cref="ICryptoTransform.TransformFinalBlock"/>
+        public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+        {
             if (_disposed)
-            {
                 throw new ObjectDisposedException(nameof(ARC4CryptoTransform),
                     DefaultFormatter.GetMessage("ObjectDisposed_Generic"));
-            }
-			if (inputBuffer == null)
-			{
-				throw new ArgumentNullException(nameof(inputBuffer));
-			}
-			if (inputOffset < 0)
-			{
-				throw new ArgumentOutOfRangeException(nameof(inputOffset), inputOffset, null);
-			}
-			if (inputCount < 0 || inputCount > inputBuffer.Length || inputBuffer.Length - inputCount < inputOffset)
-			{
-				throw new ArgumentException(null, nameof(inputCount));
-			}
-			byte[] outputBuffer = new byte[inputCount];
-			Array.Copy(inputBuffer, inputOffset, outputBuffer, 0, inputCount);
-			_arc4.Cipher(outputBuffer, 0, inputCount);
-			return outputBuffer;
-		}
+            if (inputBuffer == null)
+                throw new ArgumentNullException(nameof(inputBuffer));
+            if (inputOffset < 0)
+                throw new ArgumentOutOfRangeException(nameof(inputOffset), inputOffset, null);
+            if (inputCount < 0 || inputCount > inputBuffer.Length || inputBuffer.Length - inputCount < inputOffset)
+                throw new ArgumentException(null, nameof(inputCount));
+                
+            byte[] outputBuffer = new byte[inputCount];
+            Array.Copy(inputBuffer, inputOffset, outputBuffer, 0, inputCount);
+            _arc4.Cipher(outputBuffer, 0, inputCount);
+            return outputBuffer;
+        }
 
         /// <summary>
         ///     Reset the instance <see cref = "ARC4CryptoTransform" />
@@ -185,10 +154,8 @@ namespace System.Security.Cryptography
         public void Reset(byte[] key, ARC4SBlock sblock)
         {
             if (_disposed)
-            {
                 throw new ObjectDisposedException(nameof(ARC4CryptoTransform),
                     DefaultFormatter.GetMessage("ObjectDisposed_Generic"));
-            }
 
             _arc4 = new ARC4CryptoProvider(key, sblock);
         }
@@ -200,19 +167,19 @@ namespace System.Security.Cryptography
             _disposed = true;
         }
 
-		/// <inheritdoc cref="IDisposable.Dispose"/>
-		public void Dispose()
-		{
+        /// <inheritdoc cref="IDisposable.Dispose"/>
+        public void Dispose()
+        {
             EraseState();
-			GC.SuppressFinalize(this);
-		}
+            GC.SuppressFinalize(this);
+        }
 
         /// <summary>
         /// <inheritdoc cref="object.Finalize"/>.
         /// </summary>
-		~ARC4CryptoTransform()
-		{
+        ~ARC4CryptoTransform()
+        {
             EraseState();
-		}
-	}
+        }
+    }
 }
